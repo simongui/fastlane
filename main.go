c0f0568027fdbbe31ff35484c2e1df931e30e25c
Simon Guindon <simon@nureality.ca>
2016-08-23 02:17:25 -0400

    Update main.go

--- main.go
+++ main.go
@@ -2,35 +2,24 @@ package main
 
 import (
 	"fmt"
-	"path"
-	"runtime"
-	"strings"
 	"sync"
-	"time"
 
 	kingpin "gopkg.in/alecthomas/kingpin.v2"
 
 	"github.com/Sirupsen/logrus"
-	"github.com/rifflock/lfshook"
+	"github.com/simongui/fastlane/common"
+	"github.com/simongui/fastlane/services"
 	prefixed "github.com/x-cray/logrus-prefixed-formatter"
 )
 
 var (
-	host      = "db1.us-east-1.com"
-	port      = 33301
-	username  = "repl_user"
-	password  = "password"
-	service   *Service
-	waitGroup sync.WaitGroup
+	mysqlHost     = "db1.us-east-1.com"
+	mysqlPort     = uint16(33301)
+	mysqlUsername = "repl_user"
+	mysqlPassword = "password"
+	service       *services.ServiceHost
+	waitGroup     sync.WaitGroup
 )
-
-// CallInfo Represents caller information.
-type CallInfo struct {
-	packageName string
-	fileName    string
-	funcName    string
-	line        int
-}
 
 var (
 	verbose            = kingpin.Flag("verbose", "Verbose mode.").Short('v').Bool()
@@ -59,62 +48,22 @@ func main() {
 
 	logrus.SetFormatter(new(prefixed.TextFormatter))
 
-	logrus.StandardLogger().Hooks.Add(lfshook.NewHook(lfshook.PathMap{
-		logrus.InfoLevel:  "/var/log/fastlane.log",
-		logrus.WarnLevel:  "/var/log/fastlane.log",
-		logrus.ErrorLevel: "/var/log/fastlane.log",
-		logrus.DebugLevel: "/var/log/fastlane.log",
-		logrus.FatalLevel: "/var/log/fastlane.log",
-		logrus.PanicLevel: "/var/log/fastlane.log",
-	}))
+	// logrus.StandardLogger().Hooks.Add(lfshook.NewHook(lfshook.PathMap{
+	// 	logrus.InfoLevel:  "/var/log/fastlane.log",
+	// 	logrus.WarnLevel:  "/var/log/fastlane.log",
+	// 	logrus.ErrorLevel: "/var/log/fastlane.log",
+	// 	logrus.DebugLevel: "/var/log/fastlane.log",
+	// 	logrus.FatalLevel: "/var/log/fastlane.log",
+	// 	logrus.PanicLevel: "/var/log/fastlane.log",
+	// }))
 
 	logrus.WithFields(logrus.Fields{
-		"prefix": fmt.Sprintf("%s.%s:%d", GetCallInfo().packageName, GetCallInfo().funcName, GetCallInfo().line),
+		"prefix": fmt.Sprintf("%s.%s:%d", common.GetCallInfo().PackageName, common.GetCallInfo().FuncName, common.GetCallInfo().Line),
 	}).Info("process started")
 
-	service = &Service{}
-	go service.ListenAndServe("fastlane.db", 9999, *httpListenAddress, *redisListenAddress)
-
-	go func() {
-		startTicker()
-	}()
+	service = &services.ServiceHost{}
+	go service.ListenAndServe("fastlane.db", *httpListenAddress, *redisListenAddress, mysqlHost, mysqlPort, mysqlUsername, mysqlPassword, 9999)
 
 	waitGroup.Add(1)
 	waitGroup.Wait()
-}
-
-func startTicker() {
-	ticker := time.NewTicker(time.Second * 1)
-	for range ticker.C {
-		logrus.WithFields(logrus.Fields{
-			"prefix": fmt.Sprintf("%s.%s:%d", GetCallInfo().packageName, GetCallInfo().funcName, GetCallInfo().line),
-		}).Info(counter.Rate())
-
-		service.replicator.SaveBinlogPosition()
-		service.replicator.store.Commit()
-	}
-}
-
-// GetCallInfo Returns the caller information.
-func GetCallInfo() *CallInfo {
-	pc, file, line, _ := runtime.Caller(1)
-	_, fileName := path.Split(file)
-	parts := strings.Split(runtime.FuncForPC(pc).Name(), ".")
-	pl := len(parts)
-	packageName := ""
-	funcName := parts[pl-1]
-
-	if parts[pl-2][0] == '(' {
-		funcName = parts[pl-2] + "." + funcName
-		packageName = strings.Join(parts[0:pl-2], ".")
-	} else {
-		packageName = strings.Join(parts[0:pl-1], ".")
-	}
-
-	return &CallInfo{
-		packageName: packageName,
-		fileName:    fileName,
-		funcName:    funcName,
-		line:        line,
-	}
 }
